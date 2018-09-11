081582fe5a1380a07e9c1787e5480e10c1cf0566
CamronWarren <camron.warren27@ed.cna.nl.ca>
2018-09-11 11:09:03 -0230

    commit

--- src/Atoms/Hydrogen.java
+++ src/Atoms/Hydrogen.java
@@ -1,4 +1,21 @@
 package Atoms;
+import BuildingBlocks.*;
+
 
 public class Hydrogen {
+    private int amountOfElectrons = 1;
+    private int amountOfProtons = 1;
+    private int amountOfNeutrons = 0;
+
+    public Hydrogen(){
+        for(int i = 0; i < this.amountOfElectrons; i++){
+            Electron electron = new Electron();
+        }
+        for(int i = 0; i < this.amountOfProtons; i++){
+            Proton proton = new Proton();
+        }
+        for(int i = 0; i < this.amountOfNeutrons; i++){
+            Neutron neutron = new Neutron();
+        }
+    }
 }
